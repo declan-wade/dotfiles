bc9100ffcce5cddcf4b0f93a76fdfc447828c88d
Declan Wade <17171018+declan-wade@users.noreply.github.com>
2025-05-23 20:34:50 +0800

    Add files via upload

---
+++ brew.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# brew formulae
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+
+# Install more recent versions of some OS X tools
+brew install vim
+brew install nano
+brew install grep
+
+brew install node
+
+
+
+# to maintain cask ....
+#     brew update && brew cleanup
+
+brew install --cask 1password
+brew install --cask iterm2
+brew install --cask jordanbaird-ice
+brew install --cask aldente
+brew install --cask cyberduck
+brew install --cask zed
+brew install --cask raycast
+brew install --cask claude
+brew install --cask chatgpt
+brew install --cask appcleaner
+brew install --cask tableplus
+brew install --cask meetingbar
+brew install --cask obsidian
+brew install --cask the-unarchiver
+brew install --cask keepingyouawake
