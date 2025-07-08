c6abb763eb40c8614b158b6bfe8655555fd55500
Declan Wade <17171018+declan-wade@users.noreply.github.com>
2025-07-08 13:29:12 +0800

    Create brew-server.sh

---
+++ brew-server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# brew formulae
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+
+# Install more recent versions of some OS X tools
+brew install vim
+brew install nano
+brew install grep
+brew install node
+brew install git
+
+# to maintain cask ....
+#     brew update && brew cleanup
+
+brew install --cask iterm2
+brew install --cask appcleaner
+brew install --cask omnidisksweeper
+brew install --cask orbstack
+brew install --cask neo-network-utility
+brew install --cask jellyfin
+brew install --cask netiquette
